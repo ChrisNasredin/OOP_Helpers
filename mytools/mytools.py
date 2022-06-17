836ad83879ec70cf9146b8666ec12073ab6c8f39
ChrisNasredin <chrisnasredin@gmail.com>
2022-06-17 15:25:22 +0900

    add module mytools

--- mytools/mytools.py
+++ mytools/mytools.py
@@ -1,4 +1,8 @@
 def representationDict(dct, deep=0):
+    '''
+    Рекурсивная функция для более удобного представления словаря - показывает вложенные словари с отступами.
+    Ну и заодно учит меня работать с рекурсивными функциями.
+    '''
     if not isinstance(dct, dict):
         return f"'{dct}'," if isinstance(dct, str) else f"{dct},"
     deep += 1
@@ -9,6 +13,10 @@ def representationDict(dct, deep=0):
 
 
 def representationList(lst, deep=0):
+    '''
+        Рекурсивная функция для более удобного представления списков - показывает вложенные списки с отступами.
+        Ну и заодно учит меня работать с рекурсивными функциями.
+    '''
     if not all(map(lambda x: isinstance(x, list), lst)):
         return lst
     deep += 1
@@ -18,6 +26,10 @@ def representationList(lst, deep=0):
 
 
 def deepsearch(sequence, request, index=''):
+    '''
+        Функция, которая ищет первое вхождение во вложенных структурах и отдает их в виде индексов.
+        Я не знаю, зачем она может пригодиться, я просто захотел ее написать и написал, потому что смог.
+    '''
     results = []
     print(locals())
     for i in range(len(sequence)):
